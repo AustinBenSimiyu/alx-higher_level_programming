4f90557ce44e75e69529047355c9f94f6372b337
AustinBenSimiyu <rubberducktoo@gmail.com>
2023-10-03 17:13:42 -0700

    Second Chances

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 if number >= 0:
-    print("Last digit of {} is {} ".format(number,number % 10),end="")
+    print("Last digit of {} is {} ".format(number, number % 10), end="")
     if number % 10 > 5:
         print("and is greater than 5")
     elif number % 10 == 0:
@@ -10,5 +10,5 @@ if number >= 0:
     elif number % 10 < 6 and number % 10 != 0:
         print("and is less than 6 and not 0")
 else:
-    print("Last digit of {} is {} ".format(number,number % -10),end="")
+    print("Last digit of {} is {} ".format(number, number % -10), end="")
     print("and is less than 6 and not 0")
