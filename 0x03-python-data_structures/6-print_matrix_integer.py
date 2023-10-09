7e42e007fbf35425f0e09d47a6e8a47af7ab974d
AustinBenSimiyu <rubberducktoo@gmail.com>
2023-10-09 03:22:46 -0700

    The seventh

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in row:
+            if num != 0:
+                print("",end="")
             print("{:d}".format(num), end="")
         print()
